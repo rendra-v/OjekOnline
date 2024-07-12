69f0fb2762f7a8d070a0224168a03c1ceb2175ff
rendra <151747926+rendra-v@users.noreply.github.com>
2024-07-13 00:54:57 +0700

    Add files via upload

---
+++ Praktikum10_1402023053.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikum10_1402023053;
+
+/**
+ *
+ * @author Rafli Dika
+ */
+public class Praktikum10_1402023053 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
